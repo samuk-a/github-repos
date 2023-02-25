38beef2a88a435ad2d848afa73ee4409dd52a5d4
Samuel <samuel.santiago@ufu.br>
2023-02-25 10:54:57 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-
+import cors from 'cors';
 import router from './routes';
 
 const app = express();
 const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
